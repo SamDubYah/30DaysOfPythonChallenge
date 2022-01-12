b10fe7d68338bc489670be3d9f91a20213a47019
Sam Warner <samwarner72@gmail.com>
2022-01-11 17:58:42 -0700

    Day 17

---
+++ day_17/exceptions.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+
+print(nordic_countries)
+print(es)
+print(ru)
